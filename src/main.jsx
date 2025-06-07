ac77462e88c81396bbebb9772c870b55e10135a5
Ian Repsher <101780414+Eselin1@users.noreply.github.com>
2025-06-07 16:41:41 -0400

    Update main.jsx
    forgot ThemeToggle import

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Portfolio from './Portfolio';
 import { Analytics } from '@vercel/analytics/react';
+import ThemeToggle from './ThemeToggle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
